5652bf719335c3c4274c1f2bc9de7e1818e80870
Camilo Bravo <hola@cambraca.com>
2024-10-11 14:34:01 -0500

    Musicians Today: changes from v4.2

--- app/javascript/entrypoints/application.ts
+++ app/javascript/entrypoints/application.ts
@@ -10,6 +10,18 @@ start();
 loadPolyfills()
   .then(loadLocale)
   .then(main)
+  .then(async () => {
+    // Very hacky code for changing the mascot every once in a while.
+    async function changeMascot() {
+      if (!window.mascots)
+        window.mascots = await (await fetch('https://musicians.today/mascots.json')).json();
+
+      const index = Math.floor(Math.random() * window.mascots.length);
+      document.querySelector('.drawer__inner__mastodon img').setAttribute('src', window.mascots[index]);
+    }
+    setInterval(changeMascot, 1000*60*60);
+    window.addEventListener('load', changeMascot);
+  })
   .catch((e: unknown) => {
     console.error(e);
   });
